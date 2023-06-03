c4a70d60b1815de424b0354cf75568c777497c97
bronya <135397657+62-72-6F-6E-79-61@users.noreply.github.com>
2023-06-02 19:19:39 -0700

    Add files via upload

---
+++ background.js
@@ -0,0 +1,22 @@
+// load manifest.json
+const manifest = chrome.runtime.getManifest();
+
+// check url
+function checkUrl(url) {
+  // if the url is in host_permissions
+  for (let i = 0; i < manifest.host_permissions.length; i++) {
+    if (url.startsWith(manifest.host_permissions[i].replace("*", ""))) {
+      return true;
+    }
+  }
+  return false;
+}
+
+chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
+  if (checkUrl(tab.url)) {
+    // redirect
+    chrome.tabs.update(tabId, {
+      url: `https://bypass.city/bypass?bypass=${tab.url}`,
+    });
+  }
+});
